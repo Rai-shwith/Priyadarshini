d738f7a4274ee0ac879108eef76985208329dd3a
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-02-08 21:42:32 +0530

    chore: add new logo and make the server host to local network

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Background = ({children}) => {
   return (
-    <div className='w-screen min-h-screen bg-slate-50 flex flex-col items-center gap-6'>{children}</div>
+    <div className='w-screen min-h-screen bg-slate-50 flex flex-col items-center gap-6 overflow-x-hidden'>{children}</div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div className="w-full h-20 px-6">
       <div className="flex justify-between items-center h-full md:px-8 md:mx-8 rounded-2xl">
         <div className="size-24 flex items-end">
-          <img src="/images/logo.png" className="w-full" alt="logo" />
+          <img src="/images/logo.png" className="w-full mix-blend-multiply" alt="logo" />
         </div>
           <NavItem
             items={[
