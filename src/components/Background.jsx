516f8528d478e74444f94a84cfec53ce48479f8c
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-02-16 09:00:57 +0530

    feact: remove REACT add NEXT completely changed the project from vite + react to nextjs

--- src/components/Background.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Background = ({children}) => {
-  return (
-    <div className='w-screen min-h-screen bg-slate-100 flex flex-col items-center gap-6 overflow-x-hidden'>{children}</div>
-  )
-}
-
-export default Background
