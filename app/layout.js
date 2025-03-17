a3913e6d3dbf35f2a4476ad9ebba47fdcee38389
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-17 18:30:26 +0530

    chore: add proper asense

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
 import { Baloo_Tamma_2 } from "next/font/google";
 import Script from "next/script";
+import Head from "next/head";
 
 const balooTamma2 = Baloo_Tamma_2({
   subsets: ["kannada", "latin"],
@@ -33,9 +34,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-6603452020198848" />
-      </head>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6603452020198848"
+          crossOrigin="anonymous"
+        />
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-6603452020198848"
+        />
+      </Head>
       <body className={`${inter.variable} ${balooTamma2.variable} font-sans`}>
         <NotificationProvider>
           <Background>
